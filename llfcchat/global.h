bea51df507a8cb83054ebea2538cf6b47a42da39
howard <3524089522@qq.com>
2025-05-25 15:24:18 +0800

    day9nodejs

---
+++ llfcchat/global.h
@@ -0,0 +1,36 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+#include <QWidget>
+#include <functional>
+#include<QRegularExpression>
+#include "QStyle"
+#include <memory>
+#include <iostream>
+#include <mutex>
+#include <QByteArray>
+#include<QNetworkReply>
+#include<QJsonObject>
+#include<QDir>
+#include<QSettings>
+/**
+ * @brief repolish 用来刷新qss
+ */
+
+extern std::function<void(QWidget*)> repolish;
+
+enum ReqId{
+    ID_GET_VARIFY_CODE = 1001, //获取验证码
+    ID_REG_USER = 1002,    //注册用户
+};
+enum Modules{
+    REGISTERMOD = 0,
+};
+enum ErrorCodes{
+    SUSSESS = 0,
+    ERR_JSON = 1, //json 解析失败
+    ERR_NETWORK = 2, //网络错误
+};
+
+extern QString gate_url_prefix;
+
+#endif // GLOBAL_H
