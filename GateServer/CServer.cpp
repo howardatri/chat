313d20b488fbc7af35545bd16c2d1edb2b55ec61
howard <3524089522@qq.com>
2025-05-26 23:12:29 +0800

    day10gateconcurrent

--- GateServer/CServer.cpp
+++ GateServer/CServer.cpp
@@ -1,21 +1,24 @@
 #include "CServer.h"
 #include"HttpConnection.h"
+#include"AsioIOServicePool.h"
 CServer::CServer(boost::asio::io_context& ioc, unsigned short& port):_ioc(ioc),
-_acceptor(ioc,tcp::endpoint(tcp::v4(),port)),_socket(ioc){
+_acceptor(ioc,tcp::endpoint(tcp::v4(),port)){
 
 }
 
 void CServer::Start()
 {
 	auto self = shared_from_this();
-	_acceptor.async_accept(_socket, [self](beast::error_code ec) {
+	auto & io_context = AsioIOServicePool::GetInstance()->GetIOService();
+	std::shared_ptr<HttpConnection> new_con = std::make_shared<HttpConnection>(io_context);
+	_acceptor.async_accept(new_con->GetSocket(), [self,new_con](beast::error_code ec) {
 		try {
 			//出错放弃这链接，继续监听其他链接
 			if (ec) {
 				self->Start();
 			}
 			//创建一个新的连接，并且创建HttpConnection类管理这个链接
-			std::make_shared<HttpConnection>(std::move(self->_socket))->Start();
+			new_con->Start();
 			//继续监听
 			self->Start();
 
