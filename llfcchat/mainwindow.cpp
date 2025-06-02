09668c366fb03805db4f3a2714245f5b665ed9c6
howard <3524089522@qq.com>
2025-06-02 20:34:30 +0800

    day14registerimprovement

--- llfcchat/mainwindow.cpp
+++ llfcchat/mainwindow.cpp
@@ -7,35 +7,44 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     _login_dlg = new LoginDialog(this);
+    _login_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
     setCentralWidget(_login_dlg);
     //_login_dlg->show();
 
     //创建和注册消息链接
     connect(_login_dlg, &LoginDialog::switchRegister, this, &MainWindow::SlotSwitchReg);
-    _reg_dlg = new RegisterDialog(this);
-
-    _login_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
-    _reg_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
-    _reg_dlg->hide();
 
 }
 
 MainWindow::~MainWindow()
 {
-    // delete ui;
-    // if(_login_dlg){
-    //     delete _login_dlg;
-    //     _login_dlg = nullptr;
-    // }
-    // if(_reg_dlg){
-    //     delete _reg_dlg;
-    //     _reg_dlg = nullptr;
-    // }
+    delete ui;
 }
 
 void MainWindow::SlotSwitchReg()
 {
+    _reg_dlg = new RegisterDialog(this);
+    _reg_dlg->hide();
+    _reg_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
+
+    //连接注册界面返回登录信号
+    connect(_reg_dlg, &RegisterDialog::sigSwitchLogin, this, &MainWindow::SlotSwitchLogin);
     setCentralWidget(_reg_dlg);
     _login_dlg->hide();
     _reg_dlg->show();
+}
+
+void MainWindow::SlotSwitchLogin()
+{
+    //创建一个CentralWidget, 并将其设置为MainWindow的中心部件
+    _login_dlg = new LoginDialog(this);
+    _login_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
+    setCentralWidget(_login_dlg);
+
+    _reg_dlg->hide();
+    _login_dlg->show();
+    //连接登录界面注册信号
+    connect(_login_dlg, &LoginDialog::switchRegister, this, &MainWindow::SlotSwitchReg);
+    //连接登录界面忘记密码信号
+    // connect(_login_dlg, &LoginDialog::switchReset, this, &MainWindow::SlotSwitchReset);
 }
