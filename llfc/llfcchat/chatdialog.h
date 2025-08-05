3f62ae26a33ddacc03cd6a774643cfc13678704f
howardatri <3524089522@qq.com>
2025-08-05 10:58:05 +0800

    day26sidehubandchecklist,gg

--- llfc/llfcchat/chatdialog.h
+++ llfc/llfcchat/chatdialog.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include "global.h"
+#include "statewidget.h"
 
 namespace Ui {
 class ChatDialog;
@@ -16,15 +17,21 @@ public:
     explicit ChatDialog(QWidget *parent = nullptr);
     ~ChatDialog();
     void addChatUserList();
+    void ClearLabelState(StateWidget* lb);
 
 private:
     void ShowSearch(bool bsearch = false);
+    void AddLBGroup(StateWidget* lb);
     Ui::ChatDialog *ui;
     ChatUIMode _mode;
     ChatUIMode _state;
     bool _b_loading;
+    QList<StateWidget*> _lb_list;
 private slots:
     void slot_loading_chat_user();
+    void slot_side_chat();
+    void slot_side_contact();
+    void slot_text_changed(const QString & str);
 };
 
 #endif // CHATDIALOG_H
